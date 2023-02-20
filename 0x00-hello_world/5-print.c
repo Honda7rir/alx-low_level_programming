18ad6b614e0b40b0c6c1d5b117cd4f9c5a928bea
Honda7rir <mohnad_ayman@yahoo.com>
2023-02-20 15:21:05 -0800

    Task5

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
