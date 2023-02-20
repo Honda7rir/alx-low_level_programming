48e97810e0046f36150120bfa396c1bcc2b688d3
Honda7rir <mohnad_ayman@yahoo.com>
2023-02-20 15:12:17 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts (Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: Print "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
